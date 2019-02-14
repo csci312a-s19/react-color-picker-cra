5efae0a2e865c64db37e21f1d8b8af2e3da1c360
Michael Linderman <mlinderman@middlebury.edu>
2019-02-14 15:46:24 -0500

    Port color picker to CRA

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+function LabeledSlider(props) {
+  const labelStyle = {
+    display: 'inline-block',
+    width: '50px',
+    textAlign: 'left'
+  };
+
+  return (
+    <div>
+      <div style={labelStyle}>{props.label}:</div>
+      <input
+        type="range"
+        min="0"
+        max="255"
+        value={props.value}
+        onChange={event => props.valueChange(event.target.value)}
+      />
+      <span>{props.value}</span>
+    </div>
+  );
+}
+
+class ColorPicker extends Component {
+  constructor() {
+    super();
+    this.state = { red: 0, green: 0, blue: 0 };
+  }
+
   render() {
+    const colorBoxStyle = {
+      width: '100px',
+      height: '100px',
+      border: '1px solid black',
+      background: `rgb(${this.state.red},${this.state.green},${
+        this.state.blue
+      })`
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div id="color" style={colorBoxStyle} />
+        <LabeledSlider
+          label="Red"
+          value={this.state.red}
+          valueChange={value => {
+            this.setState({ red: value });
+          }}
+        />
+        <LabeledSlider
+          label="Green"
+          value={this.state.green}
+          valueChange={value => {
+            this.setState({ green: value });
+          }}
+        />
+        <LabeledSlider
+          label="Blue"
+          value={this.state.blue}
+          valueChange={value => {
+            this.setState({ blue: value });
+          }}
+        />
       </div>
     );
   }
+}
+
+export { ColorPicker };
+
+function App() {
+  return <ColorPicker />;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Import ColorPicker to test
+import { ColorPicker } from './App';
+
+it('Updates background color', () => {
+  const colorPicker = mount(<ColorPicker />);
+
+  // Make sure there are three sliders
+  const sliders = colorPicker.find('input[type="range"]');
+  expect(sliders).toHaveLength(3);
+
+  // The initial background color should be 0, 0, 0
+  expect(colorPicker.find('#color')).toHaveStyle('background', 'rgb(0,0,0)');
+
+  // "Move" the sliders
+  sliders.forEach(slider =>
+    slider.simulate('change', { target: { value: '100' } })
+  );
+
+  // The color box background should be set to slider values
+  expect(colorPicker.find('#color')).toHaveStyle(
+    'background',
+    'rgb(100,100,100)'
+  );
+
+  // The three numeric values should be updated
+  const values = colorPicker.find('span');
+  expect(values).toHaveLength(3);
+  values.forEach(value => {
+    expect(value.text()).toBe('100');
+  });
 });
